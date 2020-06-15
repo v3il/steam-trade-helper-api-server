e93abfe7fbdfde4e8b82c6bc83be680195a88bb0
Dima <v3il@ukr.net>
2020-06-15 15:38:20 +0300

    API for settings

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -1,13 +1,22 @@
+const fsp = require('fs').promises;
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', async (request, response) => {
-    response.json({});
+    const settings = require('../settings.json');
+    response.json(settings);
 });
 
 router.post('/', async (request, response) => {
-    response.json({ isBookmarked: true });
+    const settings = require('../settings.json');
+    const newSettings = request.body;
+    const updatedSettings = { ...settings, ...newSettings };
+
+    await fsp.writeFile(path.join(__dirname, '../settings.json'), JSON.stringify(updatedSettings));
+
+    response.json({ updated: true });
 });
 
 module.exports = router;
