06e1ce17e8b7b5719b74fb9d498d5d80bb6e52c7
Dima <v3il@ukr.net>
2020-06-17 12:45:55 +0300

    Watch flag mapping

--- src/routes/items.js
+++ src/routes/items.js
@@ -8,11 +8,14 @@ const router = express.Router();
 const mapItem = (item) => ({
     steamId: item.steam_id,
     itemName: item.item_name,
+    watch: item.watch === 1,
 });
 
 router.get('/', async (request, response) => {
     const items = await knexInstance(TABLE_NAME).select();
     const itemsMap = items.map(mapItem);
+
+    console.log(items);
 
     response.json(itemsMap);
 });
