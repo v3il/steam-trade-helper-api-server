418829841a61e5ad210a9841661aa715cc4a084b
Dima <v3il@ukr.net>
2020-06-26 17:13:34 +0300

    New table view

--- src/public/cases_page.js
+++ src/public/cases_page.js
@@ -2,27 +2,59 @@ const formatNumber = (number) => number < 10 ? `0${number}` : number;
 
 new Vue({
     el: '#app',
-    data: { items: [] },
+    data: { items: [], currentDateStr: '' },
 
     computed: {
         periodDates() {
-            if (!this.items.length) {
-                return [];
-            }
-
-            const firstItem = this.items[0];
-            return firstItem.periodsData.map(periodData => periodData.date);
+            return Array.from({ length: 11 }).map((_, index) => index - 5).map((day) => {
+                const now = Date.now();
+                const delta = 86400000 * day;
+                const changedDate = new Date(now + delta);
+                return `${formatNumber(changedDate.getDate())}.${formatNumber(changedDate.getMonth() + 1)}`;
+            });
         },
 
-        activeColumnIndex() {
-            const date = new Date();
-            const statDate = `${formatNumber(date.getDate())}.${formatNumber(date.getMonth() + 1)}`;
+        sortedItems() {
+            return this.items.sort((a, b) => {
+                const aStatForToday = a.periodsData.find(data => data.date === this.currentDateStr);
+                const bStatForToday = b.periodsData.find(data => data.date === this.currentDateStr);
 
-            return this.periodDates.findIndex(periodsDate => periodsDate === statDate);
-        },
+                if (!(aStatForToday && bStatForToday)) {
+                    return 0;
+                }
+
+                if (!aStatForToday && bStatForToday) {
+                    return -1;
+                }
+
+                if (aStatForToday && !bStatForToday) {
+                    return 1;
+                }
+
+                if (aStatForToday.prevPeriodSellPriceDiff === undefined) {
+                    return 1;
+                }
+
+                if (bStatForToday.prevPeriodSellPriceDiff === undefined) {
+                    return -1;
+                }
+
+                return bStatForToday.prevPeriodSellPriceDiff - aStatForToday.prevPeriodSellPriceDiff;
+            });
+        }
     },
 
     methods: {
+        getPriceForDate(item, date) {
+            const statData = item.periodsData.find(data => data.date === date);
+            return statData ? statData.sellPriceAvg.toFixed(2) : '&nbsp;';
+        },
+
+        getDiffForDate(item, date) {
+            const statData = item.periodsData.find(data => data.date === date);
+            return statData && statData.prevPeriodSellPriceDiff ? statData.prevPeriodSellPriceDiff : null;
+        },
+
         loadItems() {
             fetch('/cases')
                 .then(r => r.json())
@@ -31,6 +63,9 @@ new Vue({
     },
 
     created() {
+        const date = new Date();
+        this.currentDateStr = `${formatNumber(date.getDate())}.${formatNumber(date.getMonth() + 1)}`;
+
         this.loadItems();
 
         setInterval(() => {
